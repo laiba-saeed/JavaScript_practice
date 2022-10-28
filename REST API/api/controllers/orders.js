549c303dc837590adff15567b09b573c4e1cfa16
Laiba <laibasaeed538@gmail.com>
2022-10-28 13:10:08 +0500

    Create orders.js

---
+++ REST API/api/controllers/orders.js
@@ -0,0 +1,29 @@
+const order = require("../models/orders");
+
+exports.orders_get_all = (req, res, next) => {
+    Order.find()
+    .select('product quantity _id')
+    .populate('product')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+          count: docs.length,
+          orders: docs.map(doc => {
+              return {
+                  _id: doc._id,
+                  product: doc.product,
+                  quantity: doc.quantity,
+                  request: {
+                      type: 'GET',
+                      url: 'http://localhost:3000/orders' + doc._id
+                  }   
+              }
+          })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+          error: err 
+      })
+    });
+  }
